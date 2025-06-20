edd121fc783aa86bb84dc0fd164d7be0d546c9f3
shavina12 <nethupama1234@gmail.com>
2025-06-20 16:38:43 +0530

    Add dynamic template using thymleef

--- src/main/java/com/ecommercebackend/store/controller/HomeController.java
+++ src/main/java/com/ecommercebackend/store/controller/HomeController.java
@@ -3,17 +3,15 @@ package com.ecommercebackend.store.controller;
 
 import lombok.Locked;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String index(){
-        return "index.html";
+    public String index(Model model){
+        model.addAttribute("name", "Ecommerce");
+        return "index";
     }
 
-    @RequestMapping("/hello")
-    public String hello(){
-        return "index.html";
-    }
 }
