55305807c692f1184320b15dd55eec95284d414c
shavina12 <nethupama1234@gmail.com>
2025-07-05 17:41:55 +0530

    Add security config

---
+++ src/main/java/com/ecommercebackend/store/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.ecommercebackend.store.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //in here we do mainly three configuration
+        //1 stateless session is used (no cookies,just tokens)
+        //2 disable csrf -> csrf is attack where browser get tricked in to making request on behalf of a user
+        //csrf attacks security implemented for traditional web sites.fo performance issues we disable these.
+        //Authorizing routes describe which end points are are public and protected
+
+        http .sessionManagement(c ->
+                c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(c -> c.disable())
+                .authorizeHttpRequests(c->
+                        c.requestMatchers("/carts/**").permitAll()
+                                .requestMatchers(HttpMethod.POST,"/users").permitAll()
+                                .anyRequest().authenticated());
+
+
+        return http.build();
+    }
+}
